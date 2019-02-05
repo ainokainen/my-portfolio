3b97f7020e3a9a2caaf93ec3e6db4ce45407335d
ainokainen <aino.rouvinen@gmail.com>
2019-02-05 12:59:25 +0200

    Refactoring html to react components, added own images

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('Hello, loaded react dom');
+const myWork = [
+  {
+    'title': "Case Heureka",
+    'image': {
+      'desc': "example sprite of the balloonwalker",
+      'src': "images/balloonwalk.png",
+      'comment':"Animated walker to give some life to the scene."
+    }
+  },
+  {
+    'title': "Case Akseli Gallen-Kallela",
+    'image': {
+      'desc': "back of the artwork card",
+      'src': "images/teos.jpg",
+      'comment':"Back of one playing card in AKG series"
+    }
+  },
+  {
+    'title': "Gif testing for friends",
+    'image': {
+      'desc': "test gif",
+      'src': "images/discotesting3.gif",
+      'comment':"Test gif for a few friends"
+    }
+  }
+]
 
-console.log('Webpack works');
-console.log('Webpack works');
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
