051d55bc4ea9f855cb5349f7d805bd0d1964ee42
ainokainen <aino.rouvinen@gmail.com>
2019-03-01 16:37:18 +0200

    Gallery has links to images

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -5,14 +5,13 @@ import WorkList from './WorkList';
 const myWork = [
   {
     'title': "Installation in Heureka",
-    'href': "https://example.com",
     'desc': "My responsibility was to create graphics for the installation. The aim was to create a city, that the users could manipulate freely. Technologies used were The Quuppa Intelligent Locating System™ and Unity.",
     'image': {
       'desc': "Concept design for the dream city",
       'src': "images/dreamcityconcept.jpg",
       'comment':"Concept design for the dream city."
     },
-    'addtext':"Stuff about the project",
+    'addtext':"Selection of textures and images from the installation:",
     'gallery': {
       'gallerysrc':[
         {
@@ -76,14 +75,13 @@ const myWork = [
   },
   {
     'title': "Graphic design",
-    'href': "https://example.com",
     'desc': "During the years I have been doing different projects as a graphic designer. Here to showcase are few works that I have been doing for a magician and his TT13 project.",
     'image': {
       'desc': "Painted sign concept",
       'src': "images/propo2.jpg",
       'comment':"Painted sign in place"
     },
-    'addtext':"Stuff about the project",
+    'addtext':"From design to the real world",
     'gallery': {
       'gallerysrc': [
         {
@@ -103,14 +101,13 @@ const myWork = [
   },
   {
     'title': "Plan B from Outer Space",
-    'href': "https://example.com",
     'desc': "Game project concept design for a course in Metropolia. It was originally meant to be something totally different, hence the name: Plan B.",
     'image': {
       'desc': "Environment concept",
       'src': "images/redfirstdraft.jpg",
       'comment':"Test gif for a few friends"
     },
-    'addtext':"Stuff about the project",
+    'addtext':"Some early sketches of environment and enemy concepts that were later modeled and used in the actual game.",
     'gallery': {
       'gallerysrc': [
         {
@@ -119,7 +116,7 @@ const myWork = [
         },
         {
           'gallimg': "images/redfirstdraft.jpg",
-          'tooltip': "Concept design fo environment"
+          'tooltip': "Concept design for environment"
         },
         {
           'gallimg': "images/firstbug.jpg",
@@ -138,14 +135,13 @@ const myWork = [
   },
   {
     'title': "Personal projects and sketches",
-    'href': "https://example.com",
     'desc': "Some of my adventures in the digital art world.",
     'image': {
       'desc': "Collection of personal work",
       'src': "images/discotesting3.gif",
       'comment':"Test gif for a few friends"
     },
-    'addtext':"Stuff about the project",
+    'addtext':"Random selection of things that I have done for fun:",
     'gallery': {
       'gallerysrc': [
         {
